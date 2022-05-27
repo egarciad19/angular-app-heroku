2397faf25ce2f1358efc405ca4b3fec969eb9284
EDGAR FERNANDO GARCÍA DE LEÓN <egarcia147@outlook.es>
2022-05-27 13:57:41 -0600

    se modifica server.js 2

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sistema-muestras-medicas'));
+app.use(express.static('./dist'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/sistema-muestras-medicas/'}),
+    res.sendFile('index.html', {root: 'dist/'}),
 );
 
 // Start the app by listening on the default Heroku port
